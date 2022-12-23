9a291f5e5103c6aad5cb287d3c106315b0072f57
Shruti <114383443+letsbeginwithshruti@users.noreply.github.com>
2022-12-23 14:14:49 +0530

    Add files via upload

---
+++ 13 Oct 2022/ImmutableClass.java
@@ -0,0 +1,26 @@
+/*Q.3... Develop a public java class and make sure nobody can
+create any object of this class from outside the class*/
+
+public final class ImmutableClass {
+	private String name;
+	  private int date;
+	  ImmutableClass(String name, int date) {
+	  // class members are initialized using constructor
+	    this.name = name;
+	    this.date = date;
+	   }
+	    public String Name() {
+	    return name;
+	   }
+	    public int Date() {
+	    return date;
+	   }
+	}
+	class Main {
+	  public static void main(String[] args) {
+		// create object of Immutable
+	    ImmutableClass obj = new ImmutableClass("Shifa",2022);
+	    System.out.println("Name: " + obj.Name());
+	    System.out.println("Date: " + obj.Date());
+	  }
+}
